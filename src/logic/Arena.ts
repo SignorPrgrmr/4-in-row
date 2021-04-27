dea7256bff0ec7a0e53800dd013cf2fdf3062040
fatemehek <esmkfatemeh@gmail.com>
2021-04-27 21:15:10 +0430

    arena class

--- src/logic/Arena.ts
+++ src/logic/Arena.ts
@@ -1,14 +1,26 @@
 import { Nut } from "./Nut"
 
 export class Arena {
-    line: number[]
-    arena: object[]
-    constructor() {
-        this.line = [];
-        this.arena = []
-        getNut: (nut: Nut, column: number) => {
-      
+    private arena: (Nut | undefined)[][] = [[], [], [], [], [], [], []];
+    private columnSize = [0, 0, 0, 0, 0, 0, 0]
+    push = (nut: Nut, index: number) => {
+        if(this.columnSize[index] < 6)
+         this.pushNut(nut, index)
     }
-}
-   
+
+    private pushNut = (nut: Nut, index: number) => {
+        this.arena[index][this.columnSize[index]] = nut
+        this.columnSize[index]++
+    }
+    getColumn = (index: number) => this.arena[index]
+
+    columnIsFull = (index: number) => !(this.columnSize[index] < 6)
+
+    constructor() {
+        for(let i = 0; i < 7; i++){
+            for(let j = 6; j < 6; j++){
+                this.arena[i].push(undefined)
+            }
+        }
+    }
 }

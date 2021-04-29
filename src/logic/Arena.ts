0cc3e6200aa15dd13bdd8cba3284d9ca0ce49b48
Amin Farzane <signorprgrmr@gmail.com>
2021-04-29 13:16:07 +0430

    Morattab sazi

--- src/logic/Arena.ts
+++ src/logic/Arena.ts
@@ -6,8 +6,8 @@ export class Arena {
     private playerOneNuts: number
     private playerTwoNuts: number
     push = (nut: Nut, index: number) => {
-        if(this.columnSize[index] < 6)
-         this.pushNut(nut, index)
+        if (this.columnSize[index] < 6)
+            this.pushNut(nut, index)
     }
 
     private pushNut = (nut: Nut, index: number) => {
@@ -63,88 +63,13 @@ export class Arena {
     }
 
     constructor() {
-        for(let i = 0; i < 7; i++) {
-            for(let j = 0; j < 6; j++){
+        for (let i = 0; i < 7; i++) {
+            for (let j = 0; j < 6; j++) {
                 this.arena[i].push(Nut.EMPTY)
             }
         }
         this.playerOneNuts = 0
         this.playerTwoNuts = 0
     }
-    whoWin = () => {if(this.playerOneNuts == 3){
-        this.constructor()
-        return 'player_1 is winner'
-    }
-    else if(this.playerTwoNuts == 3){
-        this.constructor()
-        return 'player_2 is winner'
-    }
+
 }
-    verticalGetScore = (index: number): string => {
-            for(let i = this.columnSize[index]; i < this.columnSize[index] - 4; i--){
-                let nut1 = this.arena[index][i]
-                let nut2 = this.arena[index][i - 1]
-                if(nut1 === Nut.PLAYER_1 && nut2 === Nut.PLAYER_1)
-                    this.playerOneNuts++
-                else if(nut1 === Nut.PLAYER_2 && nut2 === Nut.PLAYER_2)
-                    this.playerTwoNuts++
-                else break
-            }
-            this.whoWin()
-            return ''
-        }
-    obliquaWin = (index: number) => {
-            if(this.columnSize[index] > 2 && index > 2){
-                  for(let i = this.columnSize[index]; i < this.columnSize[index] - 4; i--){
-                     let nut1 = this.arena[index][i]
-                     let nut2 = this.arena[index - 1][i - 1]
-                     index--
-                    if(nut1 === Nut.PLAYER_1 && nut2 === Nut.PLAYER_1)
-                        this.playerOneNuts++
-                    else if(nut1 === Nut.PLAYER_2 && nut2 === Nut.PLAYER_2)
-                        this.playerTwoNuts++
-                    else break
-                    }
-                    this.whoWin()
-                }
-             if(this.columnSize[index] < 4 && index > 2){
-                 for(let i = this.columnSize[index]; i < this.columnSize[index] - 4; i--){
-                    let nut1 = this.arena[index][i]
-                    let nut2 = this.arena[index + 1][i - 1]
-                    index++
-                    if(nut1 === Nut.PLAYER_1 && nut2 === Nut.PLAYER_1)
-                        this.playerOneNuts++
-                    else if(nut1 === Nut.PLAYER_2 && nut2 === Nut.PLAYER_2)
-                        this.playerTwoNuts++
-                    else break
-                    }
-                    this.whoWin()
-                }
-                if(this.columnSize[index] > 2 && index < 3){
-                    for(let i = this.columnSize[index]; i < this.columnSize[index] + 4; i++){
-                        let nut1 = this.arena[index][i]
-                        let nut2 = this.arena[index - 1][i + 1]
-                        index--
-                        if(nut1 === Nut.PLAYER_1 && nut2 === Nut.PLAYER_1)
-                        this.playerOneNuts++
-                    else if(nut1 === Nut.PLAYER_2 && nut2 === Nut.PLAYER_2)
-                        this.playerTwoNuts++
-                    else break
-                    }
-                    this.whoWin()
-                }
-            if(this.columnSize[index] < 4 && index < 3){
-                for(let i = this.columnSize[index]; i < this.columnSize[index] + 4; i++){
-                    let nut1 = this.arena[index][i]
-                    let nut2 = this.arena[index + 1][i + 1]
-                    index++
-                    if(nut1 === Nut.PLAYER_1 && nut2 === Nut.PLAYER_1)
-                        this.playerOneNuts++
-                    else if(nut1 === Nut.PLAYER_2 && nut2 === Nut.PLAYER_2)
-                        this.playerTwoNuts++
-                    else break
-                    }
-                    this.whoWin()
-                }    
-            }
-        }
